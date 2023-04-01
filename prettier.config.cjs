df1a07f92ac41c4d40380017ee33c919082a117b
Danni Popova <dannii.popova@gmail.com>
2023-04-01 22:09:15 +0100

    fix: add generator to package.json

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,23 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  arrowParens: "always",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: "never",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: true,
+  vueIndentScriptAndStyle: false,
 };
 
 module.exports = config;
--- src/components/plan/income/IncomeCategorySelect.tsx
+++ src/components/plan/income/IncomeCategorySelect.tsx
@@ -1,26 +1,30 @@
-import React from "react";
-import { Select } from "~/components/forms/Select";
-import { api } from "~/utils/api";
+import React from 'react';
+import { Select } from '~/components/forms/Select';
+import { api } from '~/utils/api';
 
 const IncomeCategorySelect = () => {
-  const { data } = api.subCategory.getAll.useQuery();
+	const { data } = api.subcategory.findManySubCategory.useQuery({
+		where: {
+			categoryId: 1,
+		},
+	});
 
-  const selectOptions = data?.map((cat) => ({
-    id: cat.id,
-    value: cat.name,
-    name: cat.name,
-  }));
+	const selectOptions = data?.map((cat) => ({
+		id: cat.id,
+		value: cat.name,
+		name: cat.name,
+	}));
 
-  return (
-    <div>
-      <Select
-        options={selectOptions}
-        label="Select income type"
-        name="category"
-        defaultValue="Salary"
-      />
-    </div>
-  );
+	return (
+		<div>
+			<Select
+				options={selectOptions}
+				label='Select income type'
+				name='category'
+				defaultValue='Salary'
+			/>
+		</div>
+	);
 };
 
 export default IncomeCategorySelect;
