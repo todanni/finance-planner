e368106c30278b9054343d7d27013cac0f79183c
Danni Popova <dannii.popova@gmail.com>
2023-04-04 19:38:22 +0100

    feat: list payments

--- src/components/plan/income/IncomePayments.tsx
+++ src/components/plan/income/IncomePayments.tsx
@@ -1,44 +1,29 @@
-import Table from "~/components/table";
+import Table from '~/components/table';
+import { api } from '~/utils/api';
+import { Prisma } from '@prisma/client';
 
-const rows = [
-  {
-    amount: 4596,
-    name: "HashiCorp",
-    category: "Salary",
-    date: "31-03-2023",
-  },
-  {
-    amount: 4596,
-    name: "HashiCorp",
-    category: "Salary",
-    date: "31-03-2023",
-  },
-  {
-    amount: 4596,
-    name: "HashiCorp",
-    category: "Salary",
-    date: "31-03-2023",
-  },
-  {
-    amount: 4596,
-    name: "HashiCorp",
-    category: "Salary",
-    date: "31-03-2023",
-  },
-  {
-    amount: 4596,
-    name: "HashiCorp",
-    category: "Salary",
-    date: "31-03-2023",
-  },
-];
+const headers = ['Date', 'Category', 'Name', 'Amount'];
 
 const IncomePayments = () => {
-  return (
-    <div className="relative overflow-x-auto p-2">
-      <Table headers={["Date", "Category", "Name", "Amount"]} rows={rows} />
-    </div>
-  );
+	const { data: payments, isLoading } = api.payment.listAll.useQuery();
+
+	return (
+		<div className='relative overflow-x-auto p-2'>
+			{!isLoading && (
+				<Table
+					headers={headers}
+					rows={payments?.map((payment) => {
+						return {
+							amount: new Prisma.Decimal(payment.amount).toNumber(),
+							name: payment.name,
+							category: payment.subCategoryId.toString(),
+							date: payment.createdAt.toDateString(),
+						};
+					})}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default IncomePayments;
