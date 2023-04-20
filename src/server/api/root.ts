fe64858f849237f692aa984750a2854834a700a8
Danni Popova <dannii.popova@gmail.com>
2023-04-20 22:58:21 +0100

    feat: transactions list and create

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,20 +1,8 @@
 import { createTRPCRouter } from '~/server/api/trpc';
-import { paymentsRouter } from '~/server/api/routers/payments';
-import { categoriesRouter } from './routers/categories';
-import { subCategoriesRouter } from './routers/subCategories';
-import { incomeRouter } from './routers/income';
-import { spendingRouter } from './routers/spending';
-import { debtRouter } from './routers/debt';
-import { savingsRouter } from './routers/savings';
+import { transactionsRouter } from './routers/transactions';
 
 export const appRouter = createTRPCRouter({
-	payment: paymentsRouter,
-	category: categoriesRouter,
-	subCategory: subCategoriesRouter,
-	income: incomeRouter,
-	spending: spendingRouter,
-	debt: debtRouter,
-	savings: savingsRouter,
+	transactions: transactionsRouter,
 });
 
 export type AppRouter = typeof appRouter;
