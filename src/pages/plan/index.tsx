51eb2d588fb31ee1352b54d04e44a46eb323e886
Danni Popova <dannii.popova@gmail.com>
2023-05-19 16:45:08 +0100

    refactor: package.json

--- src/pages/plan/index.tsx
+++ src/pages/plan/index.tsx
@@ -1,6 +1,4 @@
 import { type NextPage } from 'next';
-import PayForm from '~/components/payments/PayForm';
-import { PaymentsTable } from '~/components/payments/Payments';
 import { DefaultLayout } from '~/layouts/DefaultLayout';
 
 const Plan: NextPage = () => {
@@ -9,17 +7,6 @@ const Plan: NextPage = () => {
 			<div className='mt-4 rounded-xl'></div>
 		</DefaultLayout>
 	);
-};
-const showSelectedView = (view: string) => {
-	switch (view) {
-		case 'table':
-			return <PaymentsTable />;
-		case 'form':
-			return <PayForm />;
-		// case 'chart':
-		default:
-			return null;
-	}
 };
 
 export default Plan;
