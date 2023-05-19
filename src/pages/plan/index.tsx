12b258493815f080bb5baa0e1adaa3821fa66f44
Danni Popova <dannii.popova@gmail.com>
2023-05-19 16:32:32 +0100

    refactor: delete old components

--- src/pages/plan/index.tsx
+++ src/pages/plan/index.tsx
@@ -22,25 +22,4 @@ const showSelectedView = (view: string) => {
 	}
 };
 
-// const getSelectedPanel = (tab: string, dateRange: DateTime) => {
-// 	const startDate = dateRange.startOf('month').toJSDate();
-// 	const endDate = dateRange.startOf('month').toJSDate();
-
-// 	switch (tab) {
-// 		case 'Overview':
-// 			// return <OverviewPanel startDate={startDate} endDate={endDate} />;
-// 			return <PlanPanelOverview startDate={startDate} endDate={endDate} />;
-// 		case 'Income':
-// 			return <PlanPanelIncome startDate={startDate} endDate={endDate} />;
-// 		case 'Spending':
-// 			return <SpendingPanel />;
-// 		case 'Savings':
-// 			return <SavingsPanel />;
-// 		case 'Debt':
-// 			return <DebtPanel />;
-// 		default:
-// 			return <OverviewPanel startDate={startDate} endDate={endDate} />;
-// 	}
-// };
-
 export default Plan;
