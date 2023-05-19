4903fd0c49e38db65a0d3846d6071a21f45f7dea
Danni Popova <dannii.popova@gmail.com>
2023-05-19 19:01:39 +0100

    fix: footer

--- src/seed.ts
+++ src/seed.ts
@@ -4,9 +4,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { type Category, PrismaClient } from '@prisma/client';
-import { parse, stringify } from 'csv/sync';
-import * as fs from 'fs';
+import { PrismaClient } from '@prisma/client';
+// import { parse, stringify } from 'csv/sync';
+// import * as fs from 'fs';
 
 const prisma = new PrismaClient({
 	log: [
@@ -34,12 +34,12 @@ prisma.$on('query', (e) => {
 	console.log('Duration: ', e.duration, 'ms');
 });
 
-type RawRecord = {
-	Date: Date;
-	Name: string;
-	Category: string;
-	Amount: number;
-};
+// type RawRecord = {
+// 	Date: Date;
+// 	Name: string;
+// 	Category: string;
+// 	Amount: number;
+// };
 
 async function main() {
 	const sums = await prisma.transaction.groupBy({
@@ -56,66 +56,7 @@ async function main() {
 		},
 	});
 
-	type CategoryTotals = {
-		category: Category;
-		total: number;
-		subCategories: {
-			subCategoryId: number;
-			total: number;
-		}[];
-	};
-
-	const result: CategoryTotals[] = [];
-
-	// console.log(result);
-
-	// await prisma.transaction.deleteMany({
-	// 	where: {
-	// 		userId: 'clgqfe8zq0002jz34ofc2dilm',
-	// 	},
-	// });
-
-	// const content = await fs.promises.readFile('src/may_transactions.csv');
-
-	// const rawRecords = parse(content, {
-	// 	columns: true,
-	// 	cast: true,
-	// 	cast_date: true,
-	// });
-
-	// const subcategories = await prisma.subCategory.findMany({});
-
-	// const data: {
-	// 	createdAt: Date;
-	// 	name: string;
-	// 	amount: number;
-	// 	category: Category;
-	// 	subCategoryId: number;
-	// 	userId: string;
-	// }[] = [];
-
-	// rawRecords.forEach((record: RawRecord) => {
-	// 	const subcat = subcategories.find((subcategory) => {
-	// 		return subcategory.name === record.Category;
-	// 	});
-
-	// 	if (subcat) {
-	// 		data.push({
-	// 			createdAt: record.Date,
-	// 			name: record.Name,
-	// 			amount:
-	// 				subcat.name !== 'Transfers' ? Math.abs(record.Amount) : record.Amount,
-	// 			category: subcat.category,
-	// 			subCategoryId: subcat.id,
-	// 			userId: 'clgqfe8zq0002jz34ofc2dilm',
-	// 		});
-	// 	} else {
-	// 		console.log('Category not found for: ', record);
-	// 	}
-	// });
-	// await prisma.transaction.createMany({
-	// 	data: data,
-	// });
+	console.log(sums);
 }
 
 main()
